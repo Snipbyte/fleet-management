2677f0cb5e38df3359eda23a48114290b5d5c84c
Muhammad Shoaib <ms0319255@gmail.com>
2025-09-18 11:53:16 +0500

    mi

---
+++ src/app/components/common/adminHeader/adminHeader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AdminHeader({ title, showIcon }) {
+  return (
+    <div className='flex items-center gap-2'>
+      {showIcon && <img src={"/png/arrow-back.png"} className='w-8 h-8 object-contain' />}
+      <h1 className='font-semibold text-lg'>{title}</h1>
+    </div>
+  )
+}
