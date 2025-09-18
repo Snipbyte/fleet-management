028fb53b5112869302ee716a4a942600166f4c0e
husnain3003 <husnainmalik3003@gmail.com>
2025-09-18 11:26:03 +0500

    basic

---
+++ src/app/admin/settings/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AccountSettingMainPage from '../../components/admin/accountSetting/accountSettingMainPage/accountSettingMainPage'
+
+const Settings = () => {
+    return (
+        <div>
+            <AccountSettingMainPage/>
+        </div>
+    )
+}
+
+export default Settings
---
+++ src/app/components/admin/accountSetting/accountSettingMainPage/accountSettingMainPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AccountSettingMainPage = () => {
+    return (
+        <div className='px-6 py-4'>
+            
+        </div>
+    )
+}
+
+export default AccountSettingMainPage
